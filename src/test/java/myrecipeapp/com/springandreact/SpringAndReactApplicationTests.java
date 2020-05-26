504052641a786b3f3e6071e2a2a1a6d789b234f6
tangela8 <61296102+tangela8@users.noreply.github.com>
2020-05-26 00:13:05 -0400

    created frontend

---
+++ src/test/java/myrecipeapp/com/springandreact/SpringAndReactApplicationTests.java
@@ -0,0 +1,13 @@
+package myrecipeapp.com.springandreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAndReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
